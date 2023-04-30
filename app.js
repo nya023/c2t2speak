cf2198d5329ec5f9fc4b2a2897cf4773eda3c6d5
nya023 <82199346+nya023@users.noreply.github.com>
2023-04-30 22:33:30 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,57 +1,44 @@
     window.onload = function(){
-        var buf = document.querySelector('#target');
-        Tesseract.recognize(
-            buf,
-            'eng',
-            { 
-                logger: function(m) {
-                    document.querySelector('#progress').textContent = m.status;
-                }
-            }
-        )
-        .then(function(result){
-            document.querySelector('#result').textContent = result.data.text;
+        var video = document.createElement('video');
+        var canvas = document.querySelector('#canvas');
+        var buf = document.createElement('canvas');
+        document.body.append(buf);
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: 'environment'
+            },
+            audio: false
+        })
+        .then(function(stream){
+            video.srcObject = stream;
+            video.play();
+            setInterval(function(){
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
+    
+                var box = {
+                    x: 50,
+                    h: 100
+                };
+                box.y = (canvas.height - box.h) / 2;
+                box.w = (canvas.width - box.x * 2);
+        
+                ctx.beginPath();
+                ctx.strokeStyle = 'red';
+                ctx.lineWidth = 2;
+                ctx.rect(
+                    box.x, box.y, box.w, box.h
+                );
+                ctx.stroke();
+                buf.width = box.w;
+                buf.height = box.h;
+                buf.getContext('2d').drawImage(canvas, box.x, box.y, box.w, box.h, 0, 0, box.w, box.h);
+            }, 200);
+        })
+        .catch(function(e){
+            document.querySelector('#result').textContent = JSON.stringify(e);
         });
     };
 
-    var video = document.createElement('video');
-    var canvas = document.querySelector('#canvas');
-    var buf = document.createElement('canvas');
-    document.body.append(buf);
-    navigator.mediaDevices.getUserMedia({
-        video: {
-            facingMode: 'environment'
-        },
-        audio: false
-    })
-    .then(function(stream){
-        video.srcObject = stream;
-        video.play();
-        setInterval(function(){
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            var ctx = canvas.getContext('2d');
-            ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
-
-            var box = {
-                x: 50,
-                h: 100
-            };
-            box.y = (canvas.height - box.h) / 2;
-            box.w = (canvas.width - box.x * 2);
-    
-            ctx.beginPath();
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 2;
-            ctx.rect(
-                box.x, box.y, box.w, box.h
-            );
-            ctx.stroke();
-            buf.width = box.w;
-            buf.height = box.h;
-            buf.getContext('2d').drawImage(canvas, box.x, box.y, box.w, box.h, 0, 0, box.w, box.h);
-        }, 200);
-    })
-    .catch(function(e){
-        document.querySelector('#result').textContent = JSON.stringify(e);
-    });
